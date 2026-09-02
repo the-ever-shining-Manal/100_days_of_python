589ba321d6ec6ce32610e3c4bc3c5abb74fe91fa
moonal2005 <f5skmdtzgv@privaterelay.appleid.com>
2026-09-02 03:41:46 +0300

    first day done

---
+++ Day 1/band_names_generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the band names generator")
+
+city_name=input("which city did grow up in? \n")
+pet_name=input("what is the name of your pet? \n")
+print("your band name will be: " + pet_name + " " +city_name)
+
+
